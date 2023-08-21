a98087805280ca56b6d5ce0ff51d325cf88c5fd2
mcdaly <mcddaly@gmail.com>
2023-08-20 22:08:45 -0400

    Update GUI and add whisper

--- frogcast_transcription/gui_app.py
+++ frogcast_transcription/gui_app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import subprocess
+from PIL import Image, ImageTk  # Import Image and ImageTk from PIL (Pillow)
 
 
 class AudioTranscriptionApp:
@@ -8,23 +9,47 @@ class AudioTranscriptionApp:
         self.root = root
         self.root.title("Audio Transcription with Timestamps")
 
-        self.label = tk.Label(root, text="Select an audio file:")
-        self.label.pack(pady=10)
+        original_logo = Image.open("/Users/mikedaly/frogcast_transcription/daly_joe_frogcast_logo.png")
+        resized_logo = original_logo.resize((150, 150), Image.ANTIALIAS)
+        self.logo_image = ImageTk.PhotoImage(resized_logo)
+
+        self.logo_label = tk.Label(root, image=self.logo_image)
+        self.logo_label.grid(row=0, column=0, rowspan=3, padx=10, pady=10)
+
+        self.label = tk.Label(root, text="Selected audio file:")
+        self.label.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
+
+        self.audio_path_label = tk.Label(root, text="")
+        self.audio_path_label.grid(row=0, column=3, columnspan=2, padx=10, pady=10)
 
         self.browse_button = tk.Button(root, text="Browse", command=self.browse_file)
-        self.browse_button.pack()
+        self.browse_button.grid(row=1, column=1, padx=10, pady=10)
+
+        self.output_label = tk.Label(root, text="Select output folder:")
+        self.output_label.grid(row=2, column=1, padx=10, pady=10)
+
+        self.output_folder_button = tk.Button(root, text="Browse", command=self.browse_output_folder)
+        self.output_folder_button.grid(row=2, column=2, padx=10, pady=10)
 
         self.transcribe_button = tk.Button(root, text="Transcribe", command=self.transcribe_audio)
-        self.transcribe_button.pack(pady=10)
+        self.transcribe_button.grid(row=3, column=1, columnspan=2, padx=10, pady=10)
 
     def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav")])
+        file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.m4a")])
         if file_path:
             self.audio_path = file_path
+            self.audio_path_label.config(text=file_path)
+
+    def browse_output_folder(self):
+        output_folder = filedialog.askdirectory()
+        if output_folder:
+            self.output_folder = output_folder
 
     def transcribe_audio(self):
-        if hasattr(self, "audio_path"):
-            subprocess.run(["python", "transcription_script.py", self.audio_path])
+        if hasattr(self, "audio_path") and hasattr(self, "output_folder"):
+            output_filename = "transcribed_text_with_timestamps.txt"
+
+            subprocess.run(["python", "transcribe_audio_whisper.py", self.audio_path, self.output_folder, output_filename])
 
 
 if __name__ == "__main__":
---
+++ frogcast_transcription/transcribe_audio_whisper.py
@@ -0,0 +1,85 @@
+from pydub import AudioSegment
+from loguru import logger
+import whisper
+import os
+
+
+def transcribe_and_output_text(audio_path: str, output_folder: str = None, output_filename: str = None, timestamp_increment_s: float = 30):
+    transcription = transcribe_audio_with_timestamps(audio_path, timestamp_increment_s)
+
+    output_filename = "transcribed_text_with_timestamps.txt" if output_filename is None else output_filename
+    if output_folder is None:
+        output_filepath = output_filename
+    else:
+        output_filepath = os.path.join(output_folder, output_filename)
+    with open(output_filepath, "w") as output_file:
+        output_file.write(transcription)
+
+    print(f"Transcription with timestamps completed. Output saved to {output_path}")
+
+
+def transcribe_audio_with_timestamps(audio_path: str, timestamp_increment_s: float = 30):
+    # Convert m4a to wav file
+    wav_audio_file = convert_m4a_to_wav(m4a_audio_path=audio_path)
+
+    model = whisper.load_model("base")
+    result = model.transcribe(wav_audio_file)
+
+    final_text = format_transcription(transcription_data=result["segments"],
+                                      interval=timestamp_increment_s)
+    return final_text
+
+
+def format_transcription(transcription_data, interval: float = 30):
+    formatted_text = []
+    current_group = None
+    cur_time = 0
+
+    for entry in transcription_data:
+        start_time = entry['start']
+        end_time = entry['end']
+        text = entry['text']
+
+        if current_group is None:
+            current_group = {'start': start_time, 'end': end_time, 'texts': []}
+
+        if cur_time + interval < current_group['end']:
+            if current_group['texts']:
+                formatted_text.append(
+                    f"Start: {current_group['start']:.2f}s | "
+                    f"End: {current_group['end']:.2f}s | "
+                    f"Text: {' '.join(current_group['texts'])}"
+                )
+            current_group = {'start': start_time, 'end': end_time, 'texts': []}
+            cur_time = end_time
+
+        current_group['texts'].append(text)
+        current_group['end'] = end_time
+
+    if current_group['texts']:
+        formatted_text.append(
+            f"Start: {current_group['start']:.2f}s | "
+            f"End: {current_group['end']:.2f}s | "
+            f"Text: {' '.join(current_group['texts'])}"
+        )
+
+    return "\n".join(formatted_text)
+
+
+def convert_m4a_to_wav(m4a_audio_path: str, wav_audio_path: str = "converted_audio.wav"):
+    # Convert m4a to wav
+    logger.info(f"Converting {m4a_audio_path} to {wav_audio_path}")
+    audio = AudioSegment.from_file(m4a_audio_path, format="m4a")
+    audio.export(wav_audio_path, format="wav")
+    return wav_audio_path
+
+
+if __name__ == "__main__":
+    audio_path = "/Users/mikedaly/Desktop/Daly_Joe_Frogcast_Episode_0.m4a"
+    transcription = transcribe_audio_with_timestamps(audio_path)
+
+    output_path = "transcribed_text_with_timestamps.txt"
+    with open(output_path, "w") as output_file:
+        output_file.write(transcription)
+
+    print(f"Transcription with timestamps completed. Output saved to {output_path}")
