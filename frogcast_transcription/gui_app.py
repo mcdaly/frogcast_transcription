2bba36f61f4c547db1251d5a59e128965377c7af
mcdaly <mcddaly@gmail.com>
2023-08-26 21:18:35 -0400

    Add button to GUI to open text file

--- frogcast_transcription/gui_app.py
+++ frogcast_transcription/gui_app.py
@@ -5,6 +5,7 @@ import threading  # Import the threading module
 from PIL import Image, ImageTk  # Import Image and ImageTk from PIL (Pillow)
 from datetime import datetime
 import os
+import subprocess
 from frogcast_transcription.transcribe_audio_whisper import transcribe_and_output_text
 
 
@@ -47,11 +48,15 @@ class AudioTranscriptionApp:
         self.transcription_text = tk.Text(root, wrap=tk.WORD, height=10, width=60)  # Text widget for displaying transcription
         self.transcription_text.grid(row=5, column=0, columnspan=4, padx=10, pady=10, sticky="ew")  # Use sticky option to expand widget horizontally
 
+        self.open_text_button = tk.Button(root, text="Open Text File", command=self.open_text_file)
+        self.open_text_button.config(bg="light blue")  # Set button background color
+        self.open_text_button.grid(row=6, column=1, columnspan=2, padx=10, pady=10)
+
         # Adjust grid row and column weights for better layout management
-        for i in range(6):  # Number of rows
+        for i in range(7):  # Number of rows
             self.root.grid_rowconfigure(i, weight=1)
 
-        for i in range(4):  # Number of columns
+        for i in range(6):  # Number of columns
             self.root.grid_columnconfigure(i, weight=1)
 
     def browse_file(self):
@@ -61,10 +66,19 @@ class AudioTranscriptionApp:
             self.audio_path_label.config(text=file_path)
 
     def browse_output_folder(self):
-        output_folder = filedialog.askdirectory()
+        initial_dir = self.get_initial_output_directory()
+        output_folder = filedialog.askdirectory(initialdir=initial_dir)
+
         if output_folder:
             self.output_folder = output_folder
             self.output_folder_label.config(text=output_folder)
+
+    def get_initial_output_directory(self):
+        if hasattr(self, "audio_path"):
+            return os.path.dirname(self.audio_path)
+        else:
+            return os.getcwd()
+
 
     def transcribe_audio(self):
         if hasattr(self, "audio_path") and hasattr(self, "output_folder"):
@@ -94,6 +108,17 @@ class AudioTranscriptionApp:
 
         # Close the wait indicator window after the subprocess completes
         self.wait_window.destroy()
+
+    def open_text_file(self):
+        if hasattr(self, "output_folder"):
+            audio_base_name = os.path.basename(self.audio_path)
+            audio_name_without_extension = os.path.splitext(audio_base_name)[0]
+            current_date = datetime.now().strftime("%Y-%m-%d")
+            output_filename = f"{audio_name_without_extension}_transcription_{current_date}.txt"
+            text_file_path = os.path.join(self.output_folder, output_filename)
+
+            if os.path.exists(text_file_path):
+                subprocess.run(["open", text_file_path], check=True)  # Open the file with the default text editor/browser
 
 
 if __name__ == "__main__":
